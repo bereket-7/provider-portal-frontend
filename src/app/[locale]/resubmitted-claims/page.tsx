f985665bff5d6255e51c08d2f284590a55caac78
Bereket Honelign <bekhonelign@gmail.com>
2026-05-04 12:30:07 +0300

    refactor: remove resubmitted claims module and navigation link

--- src/app/[locale]/resubmitted-claims/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ResubmittedClaimsView } from "@/components/module/claims/resubmitted-claims-view";
-
-export default function ResubmittedClaimsPage() {
-	return <ResubmittedClaimsView />;
-}
